d881d8c281a6af1f2d903cf225bf3a5f672838e2
wmtorode <wmtorode@gmail.com>
2025-10-16 21:26:35 -0400

    Initial commit

---
+++ customStart/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace customStart;
+
+[BepInPlugin("ca.jwolf.customStart", "Custom Start", "1.0.0")]
+public class CustomStartPlugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin Custom Start is loaded!");
+    }
+}
---
+++ customStart/data/CustomStartConfig.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx.Configuration;
+using ProcessorTycoon.InitialData;
+
+namespace customStart.data;
+
+public static class CustomStartConfig
+{
+    public static ConfigEntry<float> StartingFunds;
+    public static ConfigEntry<int> FactorySize;
+    public static ConfigEntry<int> DifficultyLevel;
+    public static ConfigEntry<string> StartingTechLevel;
+
+    public static StartingTechnology StartingTechnology
+    {
+        get
+        {
+            StartingTechnology startingTech;
+            StartingTechnology.TryParse(StartingTechLevel.Value, out startingTech);
+            return startingTech;
+        }
+    }
+
+    public static void InitConfig(ConfigFile configFile)
+    {
+        configFile.Bind("Start", "StartingFunds", 2500000f, new ConfigDescription(
+            "Number of production planets to show. Too many and tip gets very large",
+            new AcceptableValueRange<int>(2, 35)));  
+    }
+}
